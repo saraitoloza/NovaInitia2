8157deb2b88decf1cfcbf72c91e6b5636416d27e
haileydenys <47611666+haileydenys@users.noreply.github.com>
2021-04-22 10:56:56 -0400

    Add files via upload

--- match_faces.py
+++ match_faces.py
@@ -5,8 +5,15 @@ Created on Sun Feb 21 09:18:20 2021
 @author: hailey
 """
 import math
-from collections import Counter
 import pandas as pd
+from csv import writer
+from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
+from sklearn.neighbors import KNeighborsClassifier
+import matplotlib.pyplot as plt
+import seaborn as sns
+
 
 def euclidean_distance(point1, point2):
     sum_sqrd_dist = 0
@@ -14,89 +21,85 @@ def euclidean_distance(point1, point2):
         sum_sqrd_dist += math.pow(point1[i]-point2[i], 2)
     return math.sqrt(sum_sqrd_dist)
 
-def ratio(dist1, dist2):
-    return dist1/dist2
-
-def mean(labels):
-    return sum(labels)/len(labels)
-
-def mode(labels):
-    return Counter(labels).most_common(1)[0][0]
-
-# k-NN algorithm to find matching faces
-def knn(data, query, k, distance_fn, choice_fn):
-    neighbor_dist_ind = []
+def getNeighbors(patient, df):
+       
+    # calc euclidean distance between patient and database points
+    dist_list = []
+    dfPatient = []
+    for i in range (len(df['ratio 2'])):
+        dist = euclidean_distance([patient.loc[0]['ratio 2'], patient.loc[0]['ratio 5']], [df.loc[i]['ratio 2'], df.loc[i]['ratio 5']])
+        dfPatient.append(df.loc[i]['patient'])
+        dist_list.append(dist) 
     
-    # doing for each example in the data
-    for index, example in enumerate(data):
+    data = pd.DataFrame({'patient': dfPatient, 'distance': dist_list})
+    data = data.sort_values('distance')
         
-        # calc the distance between the query example and cur example
-        distance = distance_fn(example[:-1], query)
-        
-        # add the distance and the index of the example to an ordered collection
-        neighbor_dist_ind.append((distance, index))
-        
-    # sort the ordered collection from smallest to largest
-    sorted_neighbor_dist_ind = sorted(neighbor_dist_ind)
-    
-    # pick the first k entries from sorted
-    k_nearest = sorted_neighbor_dist_ind[:k]
-    
-    # get the labels for the k entries
-    k_nearest_labels = [data[i][-1] for distance, i in k_nearest]
-    
-    return k_nearest, choice_fn(k_nearest_labels)
+    return data.head(3)
 
-# initialize k
-k = 3
+df = pd.read_csv('database.csv')
 
-# prompt user for which patient file they would like to get database for
-file = input("Enter patient file name:")
-
-# read in patient csv
-pf_pd = pd.read_csv(file + '_df.csv')
-#pf_pd = pd.read_csv('Pt1_df.csv')
-pf_pd = pf_pd.set_index('name')
-
-# pull the points from the dataframe
-ex_r = [pf_pd.loc['ex_r']['x value'], pf_pd.loc['ex_r']['y value'],pf_pd.loc['ex_r']['z value']]
-ex_l = [pf_pd.loc['ex_l']['x value'], pf_pd.loc['ex_l']['y value'],pf_pd.loc['ex_l']['z value']]
-
-ch_r = [pf_pd.loc['ch_r']['x value'], pf_pd.loc['ch_r']['y value'],pf_pd.loc['ch_r']['z value']]
-ch_l = [pf_pd.loc['ch_l']['x value'], pf_pd.loc['ch_l']['y value'],pf_pd.loc['ch_l']['z value']]
-
-en_r = [pf_pd.loc['en_r']['x value'], pf_pd.loc['en_r']['y value'],pf_pd.loc['en_r']['z value']]
-en_l = [pf_pd.loc['en_l']['x value'], pf_pd.loc['en_l']['y value'],pf_pd.loc['en_l']['z value']]
-
-tr = [pf_pd.loc['tr']['x value'], pf_pd.loc['tr']['y value'],pf_pd.loc['tr']['z value']]
-g = [pf_pd.loc['g']['x value'], pf_pd.loc['g']['y value'],pf_pd.loc['g']['z value']]
-
-sto = [pf_pd.loc['sto']['x value'], pf_pd.loc['sto']['y value'],pf_pd.loc['sto']['z value']]
-me = [pf_pd.loc['me']['x value'], pf_pd.loc['me']['y value'],pf_pd.loc['me']['z value']]
-
-n = [pf_pd.loc['n']['x value'], pf_pd.loc['n']['y value'],pf_pd.loc['n']['z value']]
-sn = [pf_pd.loc['sn']['x value'], pf_pd.loc['sn']['y value'],pf_pd.loc['sn']['z value']]
-
-zy_r = [pf_pd.loc['zy_r']['x value'], pf_pd.loc['zy_r']['y value'],pf_pd.loc['zy_r']['z value']]
-zy_l = [pf_pd.loc['zy_l']['x value'], pf_pd.loc['zy_l']['y value'],pf_pd.loc['zy_l']['z value']]
-
-# calculate the ratios of the euclidean distances and add to new dataframe
-ratios = [ ['intercanthal/interchillion', ratio(euclidean_distance(ex_r, ex_l), euclidean_distance(ch_r, ch_l))],
-          ['interchilion/interdacryon' , ratio(euclidean_distance(ch_r, ch_l), euclidean_distance(en_r, en_l))],
-          ['forehead height/stomion-soft menton', ratio(euclidean_distance(tr, g), euclidean_distance(sto, me))],
-          ['soft menton-nasion/subnasale-stomion', ratio(euclidean_distance(g, n), euclidean_distance(sn, sto))],
-          ['trichion-menton/zygions', ratio(euclidean_distance(tr, me), euclidean_distance(zy_r, zy_l))]
-          ]
-
-df = pd.DataFrame(ratios, columns = ['name', 'ratio'])
-
-
-
-
+# normalize data set
 """
-# testing
-point1 = [2,4,3]
-point2 = [3,8,2]
 
-print("{:.2f}".format(euclidean_distance(point1, point2)))
+norm_r1 = pd.DataFrame((preprocessing.normalize([np.array(df['ratio 1'])])).transpose())
+norm_r2 = pd.DataFrame((preprocessing.normalize([np.array(df['ratio 2'])])).transpose())
+norm_r3 = pd.DataFrame((preprocessing.normalize([np.array(df['ratio 3'])])).transpose())
+norm_r4 = pd.DataFrame((preprocessing.normalize([np.array(df['ratio 4'])])).transpose())
+norm_r5 = pd.DataFrame((preprocessing.normalize([np.array(df['ratio 5'])])).transpose())
+
+norm_df = pd.concat([norm_r1, norm_r2, norm_r3, norm_r4, norm_r5], axis = 1)
+
+norm_df.to_csv('norm_database.csv', index = False)
+
+
+norm_df = pd.read_csv('norm_database.csv')
 """
+
+df.head()
+df['class'].unique()
+df.isnull().values.any()
+
+df['class'] = df['class'].map({'faces-1' :0, 'faces-2' :1, 'faces-3' :2}).astype(int) #mapping numbers
+df.head()
+
+plt.close()
+sns.set_style('whitegrid');
+sns.pairplot(df, hue = 'class', height = 3);
+plt.show()
+
+sns.set_style('whitegrid');
+sns.FacetGrid(df, hue='class', size=5) \
+.map(plt.scatter, 'ratio 2', 'ratio 5') \
+.add_legend();
+plt.show()
+
+x_data = df.drop(['class', 'patient', 'ratio 4', 'ratio 3', 'ratio 1'], axis=1)
+
+y_data = df['class']
+
+# normalize data
+MinMaxScaler = preprocessing.MinMaxScaler()
+X_data_minmax = MinMaxScaler.fit_transform(x_data)
+
+data = pd.DataFrame(x_data, columns = ['ratio 2', 'ratio 5'])
+df.head()
+
+X_train, X_test, y_train, y_test = train_test_split(data, y_data, test_size=0.2, random_state = 1)
+
+knn_clf = KNeighborsClassifier()
+knn_clf.fit(X_train,y_train)
+ypred = knn_clf.predict(X_test) #These are the predicted output values
+
+result = confusion_matrix(y_test, ypred)
+print('Confusion Matrix:')
+print(result)
+result1 = classification_report(y_test, ypred)
+print('Classification Report:',)
+print (result1)
+result2 = accuracy_score(y_test,ypred)
+print('Accuracy:',result2)
+
+#some kind of action that connects with button
+curPatient = pd.read_csv('curPatient.csv')
+
+kNeighbors = getNeighbors(curPatient, df)
--- read_in.py
+++ read_in.py
@@ -6,16 +6,60 @@ Created on Tue Sep 29 13:47:46 2020
 """
 import json
 import pandas as pd
+import math
+from csv import writer
 
+# calcs the euclidean distance between 2 points
+def euclidean_distance(point1, point2):
+    sum_sqrd_dist = 0
+    for i in range(len(point1)):
+        sum_sqrd_dist += math.pow(point1[i]-point2[i], 2)
+    return math.sqrt(sum_sqrd_dist)
+
+# finds the ratio between 2 distances
+def ratio(dist1, dist2):
+    return dist1/dist2
+
+# adds a new row convereted from a list to the database csv
+def append_list_as_row(list_el):
+    with open('database.csv', 'a+', newline='') as fd:
+        write_csv = writer(fd)
+        write_csv.writerow(list_el)
+        
+# adds new patient to the database
+def addNewPatient(ratios):
+    df = pd.DataFrame(ratios, columns = ['name', 'ratio'])
+    df = df.transpose()
+    atest = df.iloc[1]
+    append_list_as_row(atest)
+
+# sets the current patient
+def setCurPatient(ratios): 
+    curPatient_df = pd.DataFrame(ratios, columns = ['name', 'ratio'])
+    curPatient_df = curPatient_df.transpose()
+    ratio_list = curPatient_df.iloc[1]
+    
+    # overwrite csv wuth new current patient
+    with open('curPatient.csv', 'w', newline='') as fd:
+        write_csv = writer(fd)
+        write_csv.writerow(['ratio 1', 'ratio 2', 'ratio 3', 'ratio 4', 'ratio 5'])
+        write_csv.writerow(ratio_list)
+        
+            
 # prompt user for which patient file they would like to get database for
 file = input("Enter patient file name:")
 
 # open patient json file 
 with open(file + '.JSON', encoding="utf8") as f:
     data = json.load(f)
- 
+
+
+
 """ measurements dataframe """
-"""
+""" 
+
+old code from semester 1
+
 # create empty dataframes for each item needed
 df = pd.DataFrame(columns = ['name'])
 #desc_df = pd.DataFrame()
@@ -68,3 +112,57 @@ features_df.to_csv(file + '_df.csv', index = False)
 #read_file = pd.read_csv(file + '_df.csv')
 #final_df.to_excel(file + '_df.xlsx', index = None, header=True)
 features_df.to_excel(file + '_df.xlsx', index = None, header=False)
+
+# read in patient csv
+pf_pd = pd.read_csv(file + '_df.csv')
+pf_pd = pf_pd.set_index('name')
+
+# pull the needed facial points from the dataframe
+ex_r = [pf_pd.loc['ex_r']['x value'], pf_pd.loc['ex_r']['y value'],pf_pd.loc['ex_r']['z value']]
+ex_l = [pf_pd.loc['ex_l']['x value'], pf_pd.loc['ex_l']['y value'],pf_pd.loc['ex_l']['z value']]
+
+ch_r = [pf_pd.loc['ch_r']['x value'], pf_pd.loc['ch_r']['y value'],pf_pd.loc['ch_r']['z value']]
+ch_l = [pf_pd.loc['ch_l']['x value'], pf_pd.loc['ch_l']['y value'],pf_pd.loc['ch_l']['z value']]
+
+en_r = [pf_pd.loc['en_r']['x value'], pf_pd.loc['en_r']['y value'],pf_pd.loc['en_r']['z value']]
+en_l = [pf_pd.loc['en_l']['x value'], pf_pd.loc['en_l']['y value'],pf_pd.loc['en_l']['z value']]
+
+tr = [pf_pd.loc['tr']['x value'], pf_pd.loc['tr']['y value'],pf_pd.loc['tr']['z value']]
+g = [pf_pd.loc['g']['x value'], pf_pd.loc['g']['y value'],pf_pd.loc['g']['z value']]
+
+sto = [pf_pd.loc['sto']['x value'], pf_pd.loc['sto']['y value'],pf_pd.loc['sto']['z value']]
+me = [pf_pd.loc['me']['x value'], pf_pd.loc['me']['y value'],pf_pd.loc['me']['z value']]
+
+n = [pf_pd.loc['n']['x value'], pf_pd.loc['n']['y value'],pf_pd.loc['n']['z value']]
+sn = [pf_pd.loc['sn']['x value'], pf_pd.loc['sn']['y value'],pf_pd.loc['sn']['z value']]
+
+zy_r = [pf_pd.loc['zy_r']['x value'], pf_pd.loc['zy_r']['y value'],pf_pd.loc['zy_r']['z value']]
+zy_l = [pf_pd.loc['zy_l']['x value'], pf_pd.loc['zy_l']['y value'],pf_pd.loc['zy_l']['z value']]
+
+# calculate the ratios of the euclidean distances and add to new dataframe
+ratios = [ ['intercanthal/interchillion', ratio(euclidean_distance(ex_r, ex_l), euclidean_distance(ch_r, ch_l))],
+          ['interchilion/interdacryon' , ratio(euclidean_distance(ch_r, ch_l), euclidean_distance(en_r, en_l))],
+          ['forehead height/stomion-soft menton', ratio(euclidean_distance(tr, g), euclidean_distance(sto, me))],
+          ['soft menton-nasion/subnasale-stomion', ratio(euclidean_distance(g, n), euclidean_distance(sn, sto))],
+          ['trichion-menton/zygions', ratio(euclidean_distance(tr, me), euclidean_distance(zy_r, zy_l))]
+          ]
+
+# prompt user to see if they want to add a new patient or set the current patient
+choice = input("Enter 1 to add a new patient to the database \nEnter 2 to set the current patient \n: ")
+
+# if & while statements to handle user choice
+while (choice != '1' or choice != '2'):
+    if choice == '1':
+        addNewPatient(ratios) # calls function
+        break
+    elif choice == '2':
+        setCurPatient(ratios) # calls function
+        break
+    else:
+        print("Invalid Input")
+    
+        # prompt user to see if they want to add a new patient or set the current patient
+        choice = input("Enter 1 to add a new patient to the database \nEnter 2 to set the current patient \n: ")
+
+
+
